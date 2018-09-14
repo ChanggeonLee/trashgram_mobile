8f8adad114b5b5ec785be8f52820f82687a5dc31
ChanggunLee <cd8114@gmail.com>
2018-09-14 15:56:11 +0900

    tap

---
+++ component/tap.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native'
+import { withNavigation } from 'react-navigation'
+
+class TapButtonGroup extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonGroup}>
+          <Button 
+            title="Home" 
+            onPress={() => this.props.navigation.navigate('Home')}
+          />
+          <Button 
+            title="City" 
+            onPress={() => this.props.navigation.navigate('City')}
+          />
+          <Button 
+            title="Upload" 
+            onPress={() => this.props.navigation.navigate('Home')}
+          />
+          <Button 
+            title="Mypage" 
+            onPress={() => this.props.navigation.navigate('Home')}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 50,
+    backgroundColor: 'white',
+  },
+  buttonGroup: {
+    flex: 1, 
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
+
+export default withNavigation (TapButtonGroup);
