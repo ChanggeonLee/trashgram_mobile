130ce566fa30ee550751da749490422a17da7b0a
ChanggunLee <cd8114@gmail.com>
2018-09-14 15:56:34 +0900

    navbar

---
+++ component/navBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native'
+import { withNavigation } from 'react-navigation'
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <View style={styles.navBar}>
+        <Text style={styles.navBarText}>
+          trashgram
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  navBar: {
+    height: 50,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  navBarText: {
+    fontSize: 20,
+  }
+});
+
+export default withNavigation (NavBar);
