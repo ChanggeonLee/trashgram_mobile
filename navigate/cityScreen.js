c220dc091b29030cd7db001fabe5847bc813ac73
ChanggunLee <cd8114@gmail.com>
2018-09-17 23:52:19 +0900

    add graph and tutorials

--- navigate/cityScreen.js
+++ navigate/cityScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native'
 
+import Graph from '../component/graph'
+
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Home',
@@ -8,17 +10,19 @@ export default class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={{flex:1, alignItems: 'center' , justifyContent: 'center'}}>
-        <Text>City Screen</Text>
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
+      // <View style={{flex:1, alignItems: 'center' , justifyContent: 'center'}}>
+      //   <Text>City Screen</Text>
+      //   <Button
+      //     title="Go to Home"
+      //     onPress={() => this.props.navigation.navigate('Home')}
+      //   />
+      //   <Button
+      //     title="Go back"
+      //     onPress={() => this.props.navigation.goBack()}
+      //   />
+      // 
+      // </View>
+      <Graph />
     );
   }
 }
