ef36fa265d48d2ed31ea730e36111f2e586ae06b
ChanggunLee <cd8114@gmail.com>
2018-09-21 12:06:01 +0900

    profile image and button

--- component/proFile.js
+++ component/proFile.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Button} from 'react-native'
+
+import ProFileImage from '../component/profileImage'
 
 export default class ProFile extends React.Component {
   render(){
     return (
       <View style={styles.container}>
-
+        <View style={{justifyContent: 'center'}}>
+          <ProFileImage />
+        </View>        
+        <View>
+          <Button title="회원 탙퇴" style={styles.button}/>
+          <Text>
+            안녕하세요 레드벨벳 덕후 이똥땅 입니다.
+          </Text>
+          <Text>
+            예리를 제일 좋아 합니다.
+          </Text>
+        </View>        
       </View>
     )
   }
@@ -15,6 +28,10 @@ const styles = StyleSheet.create({
   container:{
     marginTop:20,
     height:150,
-    backgroundColor: 'gray'
+    backgroundColor: 'gray',
+    flexDirection: 'row',
   },
+  button:{
+    // backgroundColor: ''
+  }
 });
---
+++ component/profileImage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {View, StyleSheet, Image} from 'react-native'
+
+export default class profileimage extends React.Component{
+  render(){
+    return (
+      <Image
+          style={styles.container}
+          source={require('../src/img/testImg.png')}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 100,
+    height: 100,
+    borderRadius: 100/2,
+    backgroundColor: 'red',
+  },
+});
