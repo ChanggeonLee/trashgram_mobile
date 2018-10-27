83bb96bce4d075b210e355d5d28934e0e96e4e58
ChanggunLee <cd8114@gmail.com>
2018-10-27 16:59:01 +0900

    FBlogin

--- navigate/loginScreen.js
+++ navigate/loginScreen.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+var FBLoginButton = require('../component/fbloginbutton');
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{flex:1, alignItems: 'center' , justifyContent: 'center'}}>
-        <Text>login Screen</Text>
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
+      // <View style={{flex:1, alignItems: 'center' , justifyContent: 'center'}}>
+      //   <Text>login Screen</Text>
+      //   {/* <Button
+      //     title="Go to Home"
+      //     onPress={() => this.props.navigation.navigate('Home')}
+      //   />
+      //   <Button
+      //     title="Go back"
+      //     onPress={() => this.props.navigation.goBack()}
+      //   /> */}
+      //   <Text style={styles.label}>Welcome to the Facebook SDK for React Native!</Text>
+      //   <FBLoginButton />
+      // </View>
+      <View style={styles.container}>
+       <Text style={styles.label}>Welcome to the Facebook SDK for React Native!</Text>
+       <FBLoginButton />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'normal',
+    marginBottom: 48,
+  },
+});
